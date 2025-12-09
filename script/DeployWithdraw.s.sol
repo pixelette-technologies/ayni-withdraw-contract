14f0972eafaef9dfaafd3165f015f7d0d7725005
Salman Haider <salman@pixelette.tech>
2025-12-09 15:28:07 +0500

    withdraw functions

--- script/DeployWithdraw.s.sol
+++ script/DeployWithdraw.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.28;
 
 import {Script} from "forge-std/Script.sol";
 import {Withdraw} from "../src/Withdraw.sol";
